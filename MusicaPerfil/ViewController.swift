53f494ba66af202a0836d9fae20d0b9e4f37f8fc
sofiia-0 <ana.sofiia02@gmail.com>
2024-08-17 10:38:13 -0600

    Interfaz de informacion de perfil.

--- MusicaPerfil/ViewController.swift
+++ MusicaPerfil/ViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Variables
+    var likesCount: Int = 5531
 
     //Outlet
     @IBOutlet weak var imagepfp: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         imagepfp.layer.cornerRadius = imagepfp.frame.size.width/2
         imagepfp.clipsToBounds = true
+        likeButton.setTitle("\(likesCount)", for: .normal)
     }
-
-
+    
+    @IBAction func likeUp(_ sender: Any) {
+        likesCount += 1
+        likeButton.setTitle("\(likesCount)", for: .normal)
+        
+    }
+    
 }
 
